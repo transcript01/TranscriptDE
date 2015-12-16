ef8343330153fc632af6984b5621a6e7b38c9d5a
unknown <AndreaM@TRANSCRIPT>
2015-12-16 12:46:16 +0100

    test

---
+++ Action.c
@@ -0,0 +1,11 @@
+Action()
+{
+web_service_set_option("DoNotValidateSecurity", "true");
+ 
+ 
+web_service_set_option("DoNotValidateSecurity", "B");
+ 
+ 
+web_service_set_option("A", "B");
+	return 0;
+}
